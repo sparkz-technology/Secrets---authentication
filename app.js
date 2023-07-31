326491ab88297bb0e870947564e92d9569124492
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-07-31 12:25:21 +0530

    lvl_6_jwt_cookies&bcrypt

--- app.js
+++ app.js
@@ -9,35 +9,49 @@ const express = require("express"); // to use express
 const bodyParser = require("body-parser"); // to use body parser
 const mongoose = require("mongoose"); // to use mongoose
 const ejs = require("ejs"); // to use ejs
-const session = require("express-session"); // to use session
-const passport = require("passport"); // to use passport
-const passportLocalMongoose = require("passport-local-mongoose"); // to use passport local mongoose
+const jwt = require("jsonwebtoken"); // to use jsonwebtoken
 const app = express(); // to use express
+const bcrypt = require("bcrypt"); // to use bcrypt
+const cookieParser = require("cookie-parser"); // to use cookie parser
+const { use } = require("passport");
 const port = process.env.PORT || 3000; // to use heroku port or local port
+app.use(cookieParser()); // to use cookie parser
 app.use(bodyParser.urlencoded({ extended: true })); // to use body parser
 app.use(express.static("public")); // to use static files like css, images, etc
 app.set("view engine", "ejs"); // to use ejs
-app.use(
-  session({
-    secret: "Our little secret.", // to use session
-    resave: false, // to use session// to save session
-    saveUninitialized: false, // to use session// to save session
-  }) // to use session
-);
-app.use(passport.initialize()); // to use passport// to initialize passport
-app.use(passport.session()); // to use passport// to use passport session
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
   useNewUrlParser: true, // to remove deprecation warning// to use bodyParser
 }); // to connect to mongodb
+// Function to generate JWT token
+function generateToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" }); // Token expires in 1 hour
+}
+
+const authenticateToken = (req, res, next) => {
+  //   console.log("Cookies:", req.cookies);
+  const token = req.cookies.token;
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.error("Token verification failed:", err);
+      return res.sendStatus(403);
+    }
+    console.log("Decoded user from token:", user);
+    req.user = user;
+    next();
+  });
+};
+
+// app.use(authenticateToken); // to use jwt// to authenticate token
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 }); // to create schema
-userSchema.plugin(passportLocalMongoose); // to use passport local mongoose// to hash and salt password and save it in mongodb
 const User = new mongoose.model("User", userSchema); // to create model
-passport.use(User.createStrategy()); // to use passport local mongoose// create strategy
-passport.serializeUser(User.serializeUser()); // to use passport local mongoose// create cookie
-passport.deserializeUser(User.deserializeUser()); // to use passport local mongoose// destroy cookie
 // Home route
 app.get("/", function (req, res) {
   res.render("home.ejs");
@@ -51,60 +65,59 @@ app.get("/register", function (req, res) {
   res.render("register.ejs");
 });
 // secrets route
-app.get("/secrets", function (req, res) {
-  if (req.isAuthenticated()) {
-    // to use passport local mongoose// to check if user is authenticated
-    res.render("secrets.ejs");
-  } else {
-    res.redirect("/login");
+app.get("/secrets", authenticateToken, function (req, res) {
+  // The user object from JWT is accessible in req.user due to the authenticateToken middleware
+  res.render("secrets.ejs");
+});
+
+// post route for register
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      email: username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    // to use jwt
+    const user = { username };
+    const token = generateToken(user);
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/secrets");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
   }
 });
-// post route for register
-app.post("/register", function (req, res) {
-  User.register(
-    { username: req.body.username }, // to use passport local mongoose// to register user
-    req.body.password, // to use passport local mongoose// to register user
-    function (err, user) {
-      // to use passport local mongoose// to register user
-      if (err) {
-        console.log(err); // to use passport local mongoose// to register user
-        res.redirect("/register"); // to use passport local mongoose// to register user
-      } else {
-        passport.authenticate("local")(req, res, function () {
-          // to use passport local mongoose// to register user
-          res.redirect("/secrets"); // to use passport local mongoose// to register user
-        });
-      }
-    }
-  );
-});
 // post route for login
-app.post("/login", function (req, res) {
-  const user = new User({
-    // to use passport local mongoose// to login user
-    username: req.body.username, // to use passport local mongoose// to login user
-    password: req.body.password, // to use passport local mongoose// to login user
-  });
-  req.login(user, function (err) {
-    // to use passport local mongoose// to login user
-    if (err) {
-      console.log(err); // to use passport local mongoose// to login user
-    } else {
-      passport.authenticate("local")(req, res, function () {
-        // to use passport local mongoose// to login user
-        res.redirect("/secrets"); // to use passport local mongoose// to login user
-      });
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const foundUser = await User.findOne({ email: username });
+    if (!foundUser) {
+      return res.status(404).send("User not found");
     }
-  });
+    const passwordMatch = await bcrypt.compare(password, foundUser.password);
+    if (!passwordMatch) {
+      return res.status(401).send("Incorrect password");
+    }
+    // Set the Authorization header
+    const token = generateToken({ email: foundUser.email });
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/secrets");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
+
+// Mount the Router object on the express app
 // post route for logout
 app.get("/logout", function (req, res) {
-  req.logout(function (err) {
-    if (err) {
-      console.log(err); // Handle any potential error here
-    }
-    res.redirect("/"); // Redirect after successful logout
-  });
+  res.clearCookie("token");
+  res.redirect("/");
 });
 app.listen(port, function () {
   // to listen on port
